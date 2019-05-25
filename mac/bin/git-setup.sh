b10d7b467daf481120e0debb08bff59027f46b34
PiroHiroPiro <hirohiro11071107@gmail.com>
2019-05-25 23:35:00 +0900

    add .netrc

--- mac/bin/git-setup.sh
+++ mac/bin/git-setup.sh
@@ -2,7 +2,7 @@
 
 echo "##### link git setting files ######"
 
-LINK_FILES=(.gitconfig .gitignore_global)
+LINK_FILES=(.gitconfig .gitignore_global .netrc)
 
 for file in ${LINK_FILES[@]}; do \
   unlink ~/$file&>/dev/null
