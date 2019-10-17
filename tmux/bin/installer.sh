85e3fe269a987df06bcb4dbffcc28db1e461ee6c
PiroHiroPiro <hirohiro11071107@gmail.com>
2019-10-17 21:56:58 +0900

    fix link files in tmux

--- tmux/bin/installer.sh
+++ tmux/bin/installer.sh
@@ -15,7 +15,7 @@ if test -z $(which tmux); then
 fi
 
 echo "----- link tmux setting files -----"
-LINK_FILES=(.tmux.conf)
+LINK_FILES=(.tmux.conf .config/tmux)
 for file in ${LINK_FILES[@]}; do \
   unlink ~/$file&>/dev/null
   ln -sf $(PWD)/tmux/$file ~/$file; \
--- tmux/bin/uninstaller.sh
+++ tmux/bin/uninstaller.sh
@@ -12,7 +12,7 @@ if test -n $(which tmux); then
 fi
 
 echo "----- unlink tmux setting files -----"
-LINK_FILES=(.tmux.conf)
+LINK_FILES=(.tmux.conf .config/tmux)
 for file in ${LINK_FILES[@]}; do \
   unlink ~/$file&>/dev/null; \
 done
