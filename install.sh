23f9c3361b4367ce3f74ca72958a54921acacf11
Hiroyuki Nishizawa <PiroHiroPiro@users.noreply.github.com>
2019-10-21 03:32:21 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,19 +6,12 @@ DOTPATH=~/dotfiles
 if test -n $(which git); then
   git clone --recursive "https://github.com/PiroHiroPiro/dotfiles.git" $DOTPATH
 
-# 使えない場合は curl か wget を使用する
-elif test -n $(which curl) || test -n $(which wget); then
-  if test -n $(which curl); then
-    curl -sSL "https://github.com/PiroHiroPiro/dotfiles/archive/master.tar.gz" -o $DOTPATH
-  elif test -n $(which wget); then
-    wget -O - "https://github.com/PiroHiroPiro/dotfiles/archive/master.tar.gz"
-  fi | tar zxv
+# 使えない場合は curl を使用する
+else
+  curl -sSL "https://github.com/PiroHiroPiro/dotfiles/archive/master.tar.gz" -o $DOTPATH | tar zxv
 
   # 解凍したら，DOTPATH に置く
   mv -f dotfiles-master $DOTPATH
-else
-  echo "curl or wget required"
-  exit 1
 fi
 
 cd "${DOTPATH}"
@@ -28,12 +21,12 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# makeの確認
+# make の確認
 if test -z $(which make); then
   xcode-select --install
 fi
 
-# 移動できたらmakeで実行する
+# 移動できたら make を実行する
 make init
 
 # 終了
