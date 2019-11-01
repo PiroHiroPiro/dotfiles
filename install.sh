80fb51be19c8f749dd7f025820c54872af21fcab
PiroHiroPiro <hirohiro11071107@gmail.com>
2019-11-01 19:59:23 +0900

    fix for work on Catalina

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if [ -n $(which git) ]; then
 
 # 使えない場合は curl を使用する
 elif [ -n $(which curl)]; then
-  curl -sSL "https://github.com/PiroHiroPiro/dotfiles/archive/master.tar.gz" -o $DOTPATH | tar zxv
+  curl -sSL "https://github.com/PiroHiroPiro/dotfiles/archive/master.tar.gz" | tar zxv
   # 解凍したら，DOTPATH に置く
   mv -f dotfiles-master $DOTPATH
 
--- tmux/.config/tmux/ip.sh
+++ tmux/.config/tmux/ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 version="$(sw_vers | grep ProductVersion | awk '{print $2}')"
-if [ "${version}" == "10.15" ]; then
+if [ "`echo $version | grep 10.15`" ]; then
   ipconfig getifaddr `netstat -rn -f inet | awk '/^default/{print $4;exit}'`
 else
   ipconfig getifaddr `netstat -rn -f inet | awk '/^default/{print $6;exit}'`
