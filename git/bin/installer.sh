5245308227d9e5d2bdb247523924f2c4bd2899af
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-01-03 21:24:19 +0900

    :+1: move git config/ignore/commit_template files to `~/.config/git`

--- git/bin/installer.sh
+++ git/bin/installer.sh
@@ -8,7 +8,7 @@ if [ -z $(which git) ]; then
 fi
 
 echo "----- link git setting files -----"
-LINK_FILES=(.gitconfig .gitignore_global .commit_template)
+LINK_FILES=(.config/git)
 for file in ${LINK_FILES[@]}; do \
   unlink ~/$file&>/dev/null
   ln -sf $(pwd)/git/$file ~/$file; \
