e7329692a2539c3f9a551f5729fac25b0210e79d
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-01-07 21:39:23 +0900

    :bug: set lsp install dir

--- nvim/bin/dependency-installer.sh
+++ nvim/bin/dependency-installer.sh
@@ -16,24 +16,3 @@ echo "##### install Language Server #####"
 if [ ! -d ~/langserver ]; then
   mkdir ~/langserver
 fi
-
-echo "----- install bash-language-server -----"
-if [ -z $(which bash-language-server) ]; then
-  yarn global add bash-language-server
-fi
-
-echo "----- install dockerfile-language-server -----"
-if [ -z $(which docker-langserver) ]; then
-  yarn global add dockerfile-language-server-nodejs
-fi
-
-echo "----- install yaml-language-server -----"
-if [ -e ~/langserver/yaml-language-server/out/server/src/server.js ]; then
-  echo "yaml-language-server already exists"
-else
-  cd ~/langserver && \
-    git clone https://github.com/redhat-developer/yaml-language-server.git && \
-    cd yaml-language-server && \
-    yarn && yarn run compile && \
-    cd $CURRENT_PATH
-fi
