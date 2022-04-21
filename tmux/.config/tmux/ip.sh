097f1350233110d360590613b7ad0190bff080b6
PiroHiroPiro <hirohiro11071107@gmail.com>
2022-04-22 06:07:35 +0900

    :bug: show private ip

--- tmux/.config/tmux/ip.sh
+++ tmux/.config/tmux/ip.sh
@@ -2,7 +2,7 @@
 
 #version="$(sw_vers | grep ProductVersion | awk '{print $2}')"
 #if [ "`echo $version | grep 10.15`" ]; then
-#  private_ip=$(ipconfig getifaddr `netstat -rn -f inet | awk '/^default/{print $4;exit}'`)
+  private_ip=$(ipconfig getifaddr `netstat -rn -f inet | awk '/^default/{print $4;exit}'`)
 #else
 #  private_ip=$(ipconfig getifaddr `netstat -rn -f inet | awk '/^default/{print $6;exit}'`)
 #fi
@@ -10,4 +10,4 @@
 # https://qiita.com/ksugawara61/items/887ddd1792d7d5dabb25
 public_ip=$(curl -s inet-ip.info)
 
-echo "${public_ip}"
+echo "${public_ip}(${private_ip})"
