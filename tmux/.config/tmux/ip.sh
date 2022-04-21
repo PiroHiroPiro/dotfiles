c8c0fcf5137ea71766ff2ca1ca9e758dbc206c21
PiroHiroPiro <hirohiro11071107@gmail.com>
2022-04-22 06:02:42 +0900

    :+1: show public ip instead of private ip

--- tmux/.config/tmux/ip.sh
+++ tmux/.config/tmux/ip.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
 
-version="$(sw_vers | grep ProductVersion | awk '{print $2}')"
-if [ "`echo $version | grep 10.15`" ]; then
-  ipconfig getifaddr `netstat -rn -f inet | awk '/^default/{print $4;exit}'`
-else
-  ipconfig getifaddr `netstat -rn -f inet | awk '/^default/{print $6;exit}'`
-fi
+#version="$(sw_vers | grep ProductVersion | awk '{print $2}')"
+#if [ "`echo $version | grep 10.15`" ]; then
+#  private_ip=$(ipconfig getifaddr `netstat -rn -f inet | awk '/^default/{print $4;exit}'`)
+#else
+#  private_ip=$(ipconfig getifaddr `netstat -rn -f inet | awk '/^default/{print $6;exit}'`)
+#fi
+
+# https://qiita.com/ksugawara61/items/887ddd1792d7d5dabb25
+public_ip=$(curl -s inet-ip.info)
+
+echo "${public_ip}"
