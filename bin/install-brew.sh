7d1967d23532a819c4c60b18cfec179e72e7edf4
Hiroyuki Nishizawa <PiroHiroPiro@users.noreply.github.com>
2023-07-14 16:31:50 +0900

    Update install-brew.sh

--- bin/install-brew.sh
+++ bin/install-brew.sh
@@ -2,7 +2,8 @@
 
 if [ -z $(which brew) ]; then
     echo "----- install homebrew -----"
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'If an error occurs, execute the following command: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     brew update
     brew upgrade
 fi
